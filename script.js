124ddc4b41514c0f11fdf0efe675f2b9c2ee5169
Inshra23 <164750803+Inshra23@users.noreply.github.com>
2024-06-05 15:58:53 +0500

    my practice

---
+++ script.js
@@ -0,0 +1,96 @@
+//for loops
+//flags  (in flags we use yes or no)
+// let cleanestCities = ["Calgary", "New York","Honolulu", "Adelaide", "Helsinki", "Minne", ""]
+// let cityToCheck = "New York"
+// var matchFound = "no"; 
+//  for (var i = 0; i <= 4; i++){
+//  if (cityToCheck === cleanestCities[i]) {
+//   matchFound = "yes"; 
+//   alert("It's one of the cleanest cities");
+//   }
+  
+ 
+  
+//   else if(matchFound === "no") {
+//     alert("It's not on the list"); 
+//    break;
+//   }
+// }
+
+
+
+
+//for booleans (in boolean we use true or false)
+// let cleanestCountries = ["pakistan", "india", "america", "bangladesh", "iran"];
+// let countryToCheck = "india";
+// let matchFound = false;
+
+// for (let i = 0; i < cleanestCountries; i++) {
+//     if (countryToCheck === cleanestCountries[i]) {
+//         matchFound = true;
+//         alert("It's one of the cleanest countries");
+//         break;
+//     }
+// }
+
+// if (!matchFound) {
+//     alert("It's not on the list");
+// } 
+//for arrays.length
+// let cleanestCities = ["Calgary", "New York","Honolulu", "Adelaide", "Helsinki", "Minne", ""]
+//  let cityToCheck = "New York"
+//  let matchFound = false;
+
+// for (let i = 0; i < cleanestCities.length; i++) {
+//     if (cityToCheck === cleanestCities[i]) {
+//         matchFound = true;
+//         alert("It's one of the cleanest countries");
+//         break;
+//     }
+// }
+
+// if (!matchFound) {
+//     alert("It's not on the list");
+// } 
+
+// let fruits = ["apple", "mango", "grapes", "orange", "kiwi"]
+// let fruitsToCheck = "apple"
+// let matchFound = false;
+
+// for (let i = 0; i < fruits.length; i++) {
+//     if (fruitsToCheck === fruits[i]) {
+//         matchFound = true;
+//         alert("It's one of the healthy fruit");
+//         break;
+//     }
+// }
+
+// if (!matchFound) {
+//     alert("It's not on the list");
+// } 
+
+// for(let a = 1; a <=5; a++){
+//     for(let b = 1; b <=a; b++){
+//         document.write(b + "")
+//     }
+//     document.write("<br>")
+// }
+
+
+for(a = 5; a >=1; a--){
+    for(b = a; b >= 1; b--){
+        document.write(b + "")
+    }
+    document.write("<br>")
+    }
+
+
+    for(a = 5; a >=1; a--){
+        for(b = a; b >= 1; b--){
+            document.write("*" + "")
+        }
+        document.write("<br>")
+        }
+    
+
+
